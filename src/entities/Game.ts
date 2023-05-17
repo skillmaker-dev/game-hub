70ad10741ea15f399c3db5af73262132370b2207
Anas Chahid <achahid@sqli.com>
2023-05-17 16:46:17 +0100

    Build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,4 +1,6 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 
 export interface Game
@@ -11,4 +13,6 @@ export interface Game
   rating_top: number;
   description_raw: string;
   slug: string;
+  genres: Genre[];
+  publishers: Publisher[]
 }
