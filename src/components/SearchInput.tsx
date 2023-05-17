2869d94eec542c22da58d86ed6b7cd20174256cd
Anas Chahid <achahid@sqli.com>
2023-05-17 17:47:32 +0100

    Fix Layout and navbar

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,16 +2,21 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 import useGameQueryStore from "../stores/gameQueryStore";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
   const setSearchText = useGameQueryStore((s) => s.setSearchText);
+  const navigate = useNavigate();
   const ref = useRef<HTMLInputElement>(null);
   return (
     <form
       style={{ width: "100%" }}
       onSubmit={(event) => {
         event.preventDefault();
-        if (ref.current) setSearchText(ref.current.value);
+        if (ref.current) {
+          setSearchText(ref.current.value);
+          navigate("/");
+        }
       }}>
       <InputGroup>
         <InputLeftElement children={<BsSearch />} />
