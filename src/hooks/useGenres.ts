cc4898d5cb4f62c8a9efc32ba59cc9e3812bbec6
Anas Chahid <achahid@sqli.com>
2023-05-17 15:20:39 +0100

    Refactoring: Move entities interfaces

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -3,12 +3,7 @@ import ms from "ms";
 import { CACHE_KEY_GENRES } from "../constants";
 import genres from "../data/genres";
 import APIClient, { FetchResponse } from "../services/api-client";
-export interface Genre {
-    id: number;
-    name: string;
-    image_background: string;
-}
-
+import { Genre } from "../entities/Genre";
 const apiClient = new APIClient<Genre>('/genres');
 
 
