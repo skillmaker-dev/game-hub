cbc339e9edbec08b0c3fb33795da572c71fabeee
Anas Chahid <anas.chahid777@gmail.com>
2024-01-15 23:32:11 +0100

    Update api endpoints

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,12 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import ms from "ms";
 import APIClient, { FetchResponse } from "../services/api-client";
-import  Platform  from "../entities/Platform";
+import Platform from "../entities/Platform";
 
 
-const apiClient = new APIClient<Platform>('/platforms/lists/parents');
+const apiClient = new APIClient<Platform>('/platforms');
 
-const usePlatforms = () => useQuery<FetchResponse<Platform>,Error>({
+const usePlatforms = () => useQuery<FetchResponse<Platform>, Error>({
     queryKey: ['platforms'],
     queryFn: apiClient.getAll,
     staleTime: ms('24h')
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,33 +1,29 @@
 import axios, { AxiosRequestConfig } from "axios";
 
-const axiosInstance =  axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: '410683b689cb4db5bc5e23cd979bb3ae'
-    }
+const axiosInstance = axios.create({
+    baseURL: 'https://localhost:7023/api'
 })
 
 export interface FetchResponse<T> {
     count: number;
     next: string | null;
     results: T[];
-    
+
 }
 
 class APIClient<T> {
     endpoint: string;
 
-    constructor(endpoint: string)
-    {
+    constructor(endpoint: string) {
         this.endpoint = endpoint;
     }
 
-    getAll = (config : AxiosRequestConfig) => {
-        return axiosInstance.get<FetchResponse<T>>(this.endpoint,config).then(res => res.data);
+    getAll = (config: AxiosRequestConfig) => {
+        return axiosInstance.get<FetchResponse<T>>(this.endpoint, config).then(res => res.data);
     }
 
-    getOne = (id: number | string,config?: AxiosRequestConfig) => {
-        return axiosInstance.get<T>(this.endpoint + '/' + id,config).then(res => res.data);
+    getOne = (id: number | string, config?: AxiosRequestConfig) => {
+        return axiosInstance.get<T>(this.endpoint + '/' + id, config).then(res => res.data);
     }
 }
 
