021b857feaa9dbf42726b4eec6c03be112d62858
Anas Chahid <anas.chahid777@gmail.com>
2024-01-15 23:41:21 +0100

    Check if screenshots.results is null beofre rendering

--- src/components/GameScreenshots.tsx
+++ src/components/GameScreenshots.tsx
@@ -11,9 +11,9 @@ const GameScreenshots = ({ gameId }: Props) => {
   if (error) throw error;
   return (
     <SimpleGrid spacing={2} columns={{ base: 1, md: 2 }}>
-      {screenshots.results.map((file) => (
+      {screenshots.results != null ? screenshots.results.map((file) => (
         <Image src={file.image} key={file.id} />
-      ))}
+      )) : null}
     </SimpleGrid>
   );
 };
