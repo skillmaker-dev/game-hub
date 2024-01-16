0771298dc91b0cc87b6d6d845dfc20279a3ff407
Anas Chahid <anas.chahid777@gmail.com>
2024-01-16 20:38:20 +0100

    Update SignUp function to get errors correctly

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -14,6 +14,15 @@ export interface AuthResponse {
     status: number | null
 }
 
+interface ValidationErrorResponse {
+    type: string;
+    title: string;
+    status: number;
+    errors: {
+        [key: string]: string[];
+    };
+}
+
 class AuthService {
     SignIn = (model: SignIn_SignUp): Promise<AuthResponse> => {
         return axiosAuth.post("login", model)
@@ -35,7 +44,19 @@ class AuthService {
             })
             .catch(error => {
                 // Handle error
-                return { success: false, error: error.response.data.title, status: error.response.status || 'An error occurred' } as AuthResponse;
+                if (error.response && error.response.data && error.response.data.errors) {
+                    const serverResp: ValidationErrorResponse = error.response.data
+                    const validationErrors = serverResp.errors;
+
+                    // Extract and join only the error messages with line breaks
+                    const errorMessages = Object.values(validationErrors)
+                        .map(messages => messages.join(', '))
+                        .join('\n');
+
+                    return { success: false, error: errorMessages, status: error.response.status || 'An error occurred' } as AuthResponse;
+                } else {
+                    return { success: false, error: error.response?.data.title || 'An error occurred', status: error.response?.status || 'An error occurred' } as AuthResponse;
+                }
             });
     }
 }
